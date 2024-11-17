5926919142fbeb4d401fb72f448a4a7fc31d9d4c
lyk304 <lyk304@outlook.com>
2024-11-17 21:51:20 +0800

    V0.4.1

--- mainwindow.py
+++ mainwindow.py
@@ -1,6 +1,7 @@
 from mainwindow_ui import Ui_MainWindow
 from PianoFrame import PianoFrame
-from PySide6.QtWidgets import QMainWindow, QApplication
+from PySide6.QtWidgets import QMainWindow, QApplication, QMessageBox
+from PySide6.QtGui import QIcon
 import sys
 
 
@@ -8,6 +9,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+        self.icon = QIcon("./resources/note.bmp")
+        self.setWindowIcon(self.icon)
         self.pianoFrame = PianoFrame()
         self.records = self.pianoFrame.records
         self.action_sync.triggered.connect(self.sync)
@@ -28,10 +31,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def delete(self):
         if self.listWidget.selectedIndexes():
+            warn = QMessageBox.warning(
+                self,
+                self.tr("警告"),
+                self.tr("确定要删除吗？"),
+                QMessageBox.Yes | QMessageBox.No,
+            )
+            if warn == QMessageBox.No:
+                return
             for r in self.records:
                 if r[0] == self.listWidget.selectedItems()[0].text():
                     self.records.remove(r)
-                    print(self.pianoFrame.records)
+                    # print(self.pianoFrame.records)
                     break
             self.sync()
 
